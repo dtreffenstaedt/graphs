37b6c0d5fd4e1ae8e9e3111a9bb55632d8663c3c
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-05-22 18:20:58 +0200

    Added program options to maze generation

--- src/maze.cpp
+++ src/maze.cpp
@@ -2,9 +2,28 @@
 #include "maze.h"
 
 
-auto main() -> int
+#include <boost/program_options.hpp>
+
+auto main(int argc, const char* argv[]) -> int
 {
-    constexpr std::size_t maze_dimension { 30 };
+    namespace po = boost::program_options;
+    po::variables_map options {};
+
+    po::options_description desc("General options");
+    desc.add_options()
+            ("help,h", "produce help message")
+            ("dimension,d", po::value<std::size_t>()->required(), "Dimension of maze to generate");
+
+    po::store(po::parse_command_line(argc, argv, desc), options);
+    if ((options.count("help") != 0) || (options.count("dimension") == 0)) {
+        std::cout<<desc;
+        return 0;
+    }
+    po::notify(options);
+
+
+
+    std::size_t maze_dimension { options["dimension"].as<std::size_t>() };
 
     graphs::maze m { maze_dimension, [](graphs::graph<std::size_t> g){return graphs::kruskal(std::move(g), false);}};
 
